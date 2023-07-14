7d680871d33d295a6ec8d5da73e28238c9214321
unknown <shalinirao342@gmail.com>
2023-07-14 16:13:53 +0530

    first commit

---
+++ admin/admin_panel.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+<body>
+<header class="text-gray-400 bg-gray-900 body-font">
+    <div class="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
+    <span class="ml-3 text-xl">Admin Panel</span>
+    
+    <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+        <h3>Hello, </h3><br>
+        <a href="#" class="mr-5 hover:text-white">Logout</a>
+        <a href="index1.php" class="mr-5 hover:text-white">Userpanel</a>
+      </nav></div>
+</header>
+<body >
+        <div class="container mx-auto mt-10">
+          <div class="flex shadow-md my-10">
+            <div class="w-3/4 bg-white px-10 py-10">
+              <div class="flex justify-between border-b pb-8">
+                <h1 class="font-semibold text-2xl">Data</h1>
+              </div>
+              <div class="lg:w-4/3 w-full mx-auto ">
+              <table class="table-auto w-full ">
+</body>
+</html>
