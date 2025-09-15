361920d8ba15ee2858e96a5b357ea60ea820a7c9
rao23shal <shalinirao342@gmail.com>
2025-09-15 21:59:48 +0530

    push file

--- admin/connection.php
+++ admin/connection.php
@@ -5,11 +5,15 @@
 $host = getenv('MYSQL_HOST') ?: 'localhost';
 $user = getenv('MYSQL_USER') ?: 'root';
 $pass = getenv('MYSQL_PASSWORD') ?: '';
-$db   = getenv('MYSQL_DATABASE') ?: 'user_data';
+$db   = getenv('MYSQL_DATABASE') ?: 'user_data_d3ik';
+$port = getenv('MYSQL_PORT') ?: 5432;
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
 ?>
+
+
+<!-- postgresql://root:cRkUvjM59t99sAB0iEkUQNp347DlohR6@dpg-d343daa4d50c73enf9ig-a/user_data_d3ik -->
